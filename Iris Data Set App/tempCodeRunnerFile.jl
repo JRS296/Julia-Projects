b5c2c26b34d3a46090926f45592065013be0b1bb
JRS296 <jonathansamuel296@gmail.com>
2023-07-06 18:33:03 +0530

    Commit 2 - Addition of Voyager Visualization App files - 06.07.2023 - 18:33

---
+++ Iris Data Set App/tempCodeRunnerFile.jl
@@ -0,0 +1,10 @@
+# function voyagerapp()
+#     Voyager(data)
+#     while true
+#         println("(hit Enter to close app)")
+#         input = readline()
+#         if input == ""
+#             break
+#         end
+#     end
+# end
