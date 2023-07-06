7beb9a35e3f798f5e6bceb15c25fc6b529979ac7
JRS296 <jonathansamuel296@gmail.com>
2023-07-06 19:01:41 +0530

    Commit 3 - Addition of Fluid Dynamics Visualizer App files - 06.07.2023 - 19:01

---
+++ Fluid Simulation/RecordVolume.jl
@@ -0,0 +1,34 @@
+using GLMakie
+
+function record_volume(sim, data_func;
+        name = "file.mp4",
+        duration = 1,
+        step = 0.1
+    )
+    # set up visualization data and figure
+    data = Observable(data_func(sim))
+    set_theme!(
+        theme_dark(),
+        resolution = (1080, 1080)
+    )
+    fig = volume(
+        data,
+        colorrange = (pi, 3pi),
+        algorithm = :absorption
+        #Other Options
+        #:additive :iso :absorptionrgba :indexedabsorption :mip
+    )
+    # run simulation and update figure data
+    t0 = round(sim_time(sim))
+    t = range(t0, t0 + duration; step)
+    record(fig, name, t) do ti
+        sim_step!(sim, ti)
+        data[] = data_func(sim)
+        println(
+            "simulation ",
+            round(Int, (ti - t0) / duration * 100),
+            "% complete"
+        )
+    end
+    return sim, fig
+end
